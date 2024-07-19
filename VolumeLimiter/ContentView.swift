626b7ef680d9e12e479a8ce54389fb523256e314
James Bennett <jamesa.benn@gmail.com>
2024-07-18 21:23:29 -0400

    Initial Commit

---
+++ VolumeLimiter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VolumeLimiter
+//
+//  Created by James Bennett on 7/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VolumeLimiter/VolumeLimiterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeLimiterApp.swift
+//  VolumeLimiter
+//
+//  Created by James Bennett on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct VolumeLimiterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
